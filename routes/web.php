3a344ef154bb5fd418b1d6a9c27a5384a7442f8c
rnehring <nehring.ryan@gmail.com>
2026-04-06 02:18:16 -0400

    fix root route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn() => redirect()->route('login'));
 // Auth routes
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
